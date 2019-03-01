f78d2a4c9df11c3894f757396d28d8befa9c74c9
Thirajade Lekkunlawat <thirajade_lek@truecorp.co.th>
2019-03-01 10:20:47 +0700

    add release-plugin-demo

---
+++ release-plugin-demo/src/main/java/com/thirajade/releaseplugindemo/ReleasePluginDemoApplication.java
@@ -0,0 +1,17 @@
+package com.thirajade.releaseplugindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class ReleasePluginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasePluginDemoApplication.class, args);
+	}
+
+}
+
